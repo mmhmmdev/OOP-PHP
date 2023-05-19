068765b51b78d079feb062952ed450379b2f3c75
mmhmmdev <m.mhmmd.ev@gmail.com>
2023-05-19 20:09:19 +0300

    Create New Object

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php
+/*
+        Classe & Object
+        - Class in a Blueprint That Can Create Object From
+        - Objcet is A Member in The Main Application
+        
+        Apple 
+        - Apple Store                = Application
+        - Iphone & labtop            = Object
+        - Apple Blueprint Design     = Class
+
+        Web Application
+        - Web Application            = Application
+        - The Members                = Object
+        - Code To Add New Member     = Class
+        
+        Blog System
+        - Blog System                = Application
+        - Blog                       = Object
+        - Code To Add New Post & News & Article = Class
+        
+ */
 
 
+class appleDevice
+{
+    
+}
 
+    $iphone6Plus = new  appleDevice();
+    
+    echo '<per>';
+    var_dump($iphone6Plus);
+    echo '<per>';
+    
 ?>
