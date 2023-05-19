e2ffb5968070634540b1dd4b6b3ce02616407e02
mmhmmdev <m.mhmmd.ev@gmail.com>
2023-05-19 20:48:36 +0300

    Create Properties

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
         Classe & Object
         - Class in a Blueprint That Can Create Object From
         - Objcet is A Member in The Main Application
+        - Class Has Properties & Methods
+        - Variable Inside Class = [ Property ]
+        - Variable Outside Class = [ Variable ]
+        
+        - [ class ] = Class Keyword
+        - [ new ] = New Object
+        - [ Public, Private, Protected ] = Visibility Markers 
+        - [ -> ] = Object Operator
         
         Apple 
         - Apple Store                = Application
@@ -24,13 +32,34 @@
 
 class appleDevice
 {
+    //Properties
     
+    public $ram = '2GB';
+    public $inch = '5.5';
+    public $space = '50GB';
+    public $color =  'Red';
 }
 
     $iphone6Plus = new  appleDevice();
-    
+    $iphone6Plus->ram = "3GB";
+    $iphone6Plus->inch = "5.5";
+    $iphone6Plus->space = "32GB";
+    $iphone6Plus->color = "Red";
     echo '<per>';
     var_dump($iphone6Plus);
     echo '<per>';
+
+    $iphone7Plus = new  appleDevice();
+    $iphone7Plus->ram = "5GB";
+    $iphone7Plus->inch = "8.5";
+    $iphone7Plus->space = "64GB";
+    $iphone7Plus->color = "black";
+    echo '<per>';
+    var_dump($iphone7Plus);
+    echo '<per>';
     
+    $iphone = new  appleDevice();
+    echo '<per>';
+    var_dump($iphone);
+    echo '<per>';
 ?>
